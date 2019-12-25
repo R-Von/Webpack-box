b9278c986658427ab415f6b566e2e08d0d15ec6a
R-Von <778615648@qq.com>
2019-12-25 17:32:40 +0800

    feat: add new

---
+++ vue-webpack/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <!-- Hello Vue && Webpack  -->
+    {{tags}}
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      tags:'Hello Vue'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
